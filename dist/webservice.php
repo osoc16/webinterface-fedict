744adbbe4d84c20c2d9854343a47a48cfdc94025
Izoman <umut.saglam1@hotmail.com>
2016-07-18 16:05:04 +0200

    Updated front-end + added base for data display + gitignore

--- dist/webservice.php
+++ dist/webservice.php
@@ -40,29 +40,18 @@
     // URL structure: baseURI/servicefamily/service#method
     // Example:
     // http://localhost/webinterface-fedict/dist/?serviceFamily=TestServiceFamily&service=TestService&method=TestMethod
-    //$serviceFamily = isset($_GET['serviceFamily']) ? $_GET['serviceFamily'] : '%';
-    //$service = isset($_GET['service']) ? $_GET['service'] : '%';
-    //$method = isset($_GET['method']) ? $_GET['method'] : '%';
-
-    $catalog = isset($_POST['catalog']) ? $_POST['catalog'] : '';
-    $dataset = isset($_POST['dataset']) ? $_POST['dataset'] : '';
-    $distribution = isset($_POST['distribution']) ? $_POST['distribution'] : '';
-
-	$services = array(
-        array('name' => "PersonService",'description' => "Some description is here....", 'method' => "GetPersonDetails"),
-        array('name' => "SomeService",'description' => "Second desc is here....", 'method' => "GetServicex")
-
-    );
+    $name = isset($_GET['name']) ? $_GET['name'] : '';
+	$description = isset($_GET['description']) ? $_GET['description'] : '';
+	$method = isset($_GET['method']) ? $_GET['method'] : '';
 
 	// load template
-	$tpl = $twig->loadTemplate('index.twig');
+	$tpl = $twig->loadTemplate('webservice.twig');
 
 	// render template with our data
 	echo $tpl->render(array(
-		'services' => $services,
-        'catalog'  => $catalog,
-        'dataset'  => $dataset,
-        'distribution'  => $distribution,
+		'name'  => $name,
+		'description'  => $description,
+		'method'  => $method,
     ));
 
 
