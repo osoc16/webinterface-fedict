1f981013ff727dd7ad41680d60c9ed859f7030b1
Izoman <umut.saglam1@hotmail.com>
2016-07-19 11:24:56 +0200

    Working base with server

--- dist/scripts/js/main.js
+++ dist/scripts/js/main.js
@@ -1,6 +1,6 @@
 var ldf = require('ldf-client');
 var fragmentsClient = new ldf.FragmentsClient('http://fragments.dbpedia.org/2014/en');
 
-var query = 'SELECT * { ?s ?p <http://dbpedia.org/resource/Belgium>. ?s ?p ?o } LIMIT 100',
+var query = 'SELECT * { ?soj ?p <http://dbpedia.org/resource/Belgium>. ?s ?p ?o } LIMIT 100',
     results = new ldf.SparqlIterator(query, { fragmentsClient: fragmentsClient });
 results.on('data', console.log);
